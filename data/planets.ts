991bdded09201eedc44ad5d666fed4cbefa3ac60
Dari <darmcq8632@ecsk12.org>
2026-02-23 00:45:26 -0600

    planets name change

--- data/planets.ts
+++ data/planets.ts
@@ -1,10 +1,10 @@
 export const planets = [
-    { name: "Mercury", texture: "/textures/mercury.jpg", size: 0.4, distance: 8, rotationSpeed: 0.02 },
-    { name: "Venus", texture: "/textures/venus.jpg", size: 0.9, distance: 12, rotationSpeed: 0.015 },
-    { name: "Earth", texture: "/textures/earth.jpg", size: 1, distance: 16, rotationSpeed: 0.01 },
-    { name: "Mars", texture: "/textures/mars.jpg", size: 0.5, distance: 21, rotationSpeed: 0.008 },
-    { name: "Jupiter", texture: "/textures/jupiter.jpg", size: 3, distance: 35, rotationSpeed: 0.005 },
-    { name: "Saturn", texture: "/textures/saturn.jpg", size: 2.5, distance: 50, rotationSpeed: 0.004 },
-    { name: "Uranus", texture: "/textures/uranus.jpg", size: 2, distance: 65, rotationSpeed: 0.003 },
-    { name: "Neptune", texture: "/textures/neptune.jpg", size: 2, distance: 80, rotationSpeed: 0.002 },
+    { name: "Mercury", texture: "/textures/Mercury.jpg", size: 0.4, distance: 8, rotationSpeed: 0.02 },
+    { name: "Venus", texture: "/textures/Venus.jpg", size: 0.9, distance: 12, rotationSpeed: 0.015 },
+    { name: "Earth", texture: "/textures/Earth.jpg", size: 1, distance: 16, rotationSpeed: 0.01 },
+    { name: "Mars", texture: "/textures/Mars.jpg", size: 0.5, distance: 21, rotationSpeed: 0.008 },
+    { name: "Jupiter", texture: "/textures/Jupiter.jpg", size: 3, distance: 35, rotationSpeed: 0.005 },
+    { name: "Saturn", texture: "/textures/Saturn.jpg", size: 2.5, distance: 50, rotationSpeed: 0.004 },
+    { name: "Uranus", texture: "/textures/Uranus.jpg", size: 2, distance: 65, rotationSpeed: 0.003 },
+    { name: "Neptune", texture: "/textures/Neptune.jpg", size: 2, distance: 80, rotationSpeed: 0.002 },
 ]
